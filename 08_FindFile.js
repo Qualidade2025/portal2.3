fdbd4fe35574c30bfa26c2a80672ffd70276d65c
Qualidade2025 <baldiqualidade@gmail.com>
2026-04-27 11:20:26 -0300

    Update 08_FindFile.js

--- 08_FindFile.js
+++ 08_FindFile.js
@@ -6,6 +6,11 @@ function _findRncFile_(rncId, ano, mesNome) {
   var base = _getBaseFolderFromDados_();
   if (!base) throw new Error('PastaBaseDriveId/PastaBaseDrive não definida na aba Dados.');
 
+  function getFileByIdSafe_(fileId) {
+    if (!fileId) return null;
+    try { return DriveApp.getFileById(fileId); } catch (_) { return null; }
+  }
+
   function locateInRncFolder_(rncFolder, monthFolder){
     if (!rncFolder) return null;
     var it = rncFolder.getFilesByName(rncId + '.json');
@@ -42,13 +47,32 @@ function _findRncFile_(rncId, ano, mesNome) {
   var idx = _getRncFolderIndex_(base);
   var cached = idx[rncId];
   if (cached) {
+    var cachedByJsonId = getFileByIdSafe_(cached.jsonrncid);
+    if (cachedByJsonId) {
+      var parentIt = cachedByJsonId.getParents();
+      var jsonFolder = parentIt.hasNext() ? parentIt.next() : null;
+      var jsonMonth = jsonFolder ? (function() {
+        var pit = jsonFolder.getParents();
+        return pit.hasNext() ? pit.next() : null;
+      })() : null;
+      _registerRncFolders_(base, rncId, jsonMonth, jsonFolder);
+      _registerRncJsonInIndex_(base, rncId, cachedByJsonId);
+      return { file: cachedByJsonId, monthFolder: jsonMonth, rncFolder: jsonFolder };
+    }
+
     var cachedMonth = _safeGetFolderById_(cached.monthFolderId);
     var cachedRnc = _safeGetFolderById_(cached.rncFolderId);
     var cachedHit = locateInRncFolder_(cachedRnc, cachedMonth);
-    if (cachedHit) return cachedHit;
+    if (cachedHit) {
+      _registerRncJsonInIndex_(base, rncId, cachedHit.file);
+      return cachedHit;
+    }
     if (cachedMonth) {
       var monthHit = locateInMonth_(cachedMonth);
-      if (monthHit) return monthHit;
+      if (monthHit) {
+        _registerRncJsonInIndex_(base, rncId, monthHit.file);
+        return monthHit;
+      }
     }
   }
 
@@ -59,7 +83,10 @@ function _findRncFile_(rncId, ano, mesNome) {
       var monthFolder = year.getFoldersByName(mesNome);
       if (monthFolder.hasNext()) {
         var monthHit = locateInMonth_(monthFolder.next());
-        if (monthHit) return monthHit;
+        if (monthHit) {
+          _registerRncJsonInIndex_(base, rncId, monthHit.file);
+          return monthHit;
+        }
       }
     }
   }
@@ -71,7 +98,10 @@ function _findRncFile_(rncId, ano, mesNome) {
     while (mIt.hasNext()) {
       var mFolder = mIt.next();
       var found = locateInMonth_(mFolder);
-      if (found) return found;
+      if (found) {
+        _registerRncJsonInIndex_(base, rncId, found.file);
+        return found;
+      }
     }
   }
   return null;
